a38d8f5077427ab872d7d72869ba6ecb18fa2633
guitardan <66696600+guitardan@users.noreply.github.com>
2023-09-10 23:18:46 +0200

    feat: support start/endpoints for Python looping

--- loop.py
+++ loop.py
@@ -5,33 +5,40 @@ import sounddevice as sd
 def get_samplerate():
     output_device = sd.query_devices(kind='output')
     try:
-        return output_device['default_samplerate']
+        return int(output_device['default_samplerate'])
     except TypeError:
-        print('no output device available, terminating...')
-        sys.exit(0)
+        sys.exit('no output device available')
     except KeyError:
-        print('no default samplerate available, terminating...')
-        sys.exit(0)
+        sys.exit('no default samplerate available')
 
 def callback(indata, outdata, frames, time, status):
-    global loop_idx
+    global widx, ridx
     if status:
         print(status)
-    if loop_idx < len(loop):
-        loop[loop_idx%len(loop):loop_idx%len(loop) + frames] = indata
+
+    if is_recording:
         outdata[:] = indata
+        loop[widx:widx + frames] = indata
+        widx += frames
+        if widx > len(loop):
+            raise Exception(f'maximum loop length of {max_loop_duration_s}s exceeded')
     else:
-        outdata[:] = indata + loop[loop_idx%len(loop):loop_idx%len(loop) + frames]
-    loop_idx += frames
+        if widx > 0:
+            outdata[:] = indata + loop[ridx%widx:ridx%widx + frames]
+            ridx += frames
+        else:
+            outdata[:] = indata
 
 def run_ui():
+    global is_recording
     while True:
         txt = input()
         if txt.casefold() == 'q':
             stream.stop()
             sys.exit(0)
         elif txt == '':
-            print('enter')
+            is_recording = ~is_recording
+            print('recording...' if is_recording else '...stopped')  
 
 def play_audio():
     with stream:
@@ -39,12 +46,12 @@ def play_audio():
             if stream.stopped:
                 break
 
-n_frames = 512
+max_loop_duration_s = 30
 samplerate  = get_samplerate()
 stream = sd.Stream(samplerate=samplerate, callback=callback)
 
-loop = np.zeros((int(samplerate / n_frames) * n_frames, 2))
-loop_idx, is_recording = 0, False
+loop = np.zeros((samplerate * max_loop_duration_s, 2))
+widx, ridx, is_recording = 0, 0, False
 if __name__ == "__main__":
     threading.Thread(target=play_audio).start()
     threading.Thread(target=run_ui).start()
